588543c443569038237db2b73f231db29e3161f0
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-12 10:08:02 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created 
+      render json: @user, status: :created
     else
       render json:{ status: 'no'}
     end
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :age)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
