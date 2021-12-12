daf85677860412ec2c60a46a637517d7a506daf8
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-12 12:10:06 +0900

    omniauth_not_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
 
-  get 'users/index'
-  get 'users/show'
-  resources :users, only: [:index, :show, :create]
+  # get 'users/index'
+  # get 'users/show'
+  # resources :users, only: [:index, :show, :create]
 
   namespace :api, format: 'json' do
     resources :memos, only: [:index]
